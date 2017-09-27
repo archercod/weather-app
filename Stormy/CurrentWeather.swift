49877149d12b88caa89c8c76f8febdf91514938b
marcinoc <pietrzakmarcin@hotmail.com>
2017-09-27 22:56:22 +0200

    Start to create model for CurrentWeater and create enum for manage WeatherIcon

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Marcin Pietrzak on 27.09.2017.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: String
+}
